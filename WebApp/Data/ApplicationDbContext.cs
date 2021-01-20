5c5ec2ec1c43b3dd30d48ce56b670f1471ca3eb0
abshek2109 <abhishekpandita18@gmail.com>
2021-01-20 18:54:13 +0530

    first commit

---
+++ WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebApp.Models.Joke> Joke { get; set; }
+    }
+}
---
+++ WebApp/Models/Joke.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
